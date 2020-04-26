ca47ca13ce862b8e814c5b663e68431ce576ea9c
singh-ashok25 <ashok_iitr@yahoo.com>
2020-04-26 02:40:23 +0100

    add files

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum -y install docker
+sudo systemctl start docker 
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+yum install git -y
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
