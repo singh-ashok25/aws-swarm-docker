5d7bb7b2b51d1198d87bc409c74d89cd21a2c0bb
singh-ashok25 <ashok_iitr@yahoo.com>
2020-04-26 18:32:25 +0100

    update region variable

--- variables.sh
+++ variables.sh
@@ -8,6 +8,6 @@ sg_name="swarm"
 # AWS 
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
-export AWS_DEFAULT_REGION="eu-west-2"
-export region="eu-west-2"
+export region=`cat ~/.aws/config | grep region | cut -d "=" -f2 | cut -d " " -f2`
+export AWS_DEFAULT_REGION=$region
 
